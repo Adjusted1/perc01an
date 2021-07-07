03eeda462f12b6877f1790106f59c9a2b45511ab
unknown <sweetlawrence205@gmail.com>
2021-07-07 13:30:32 -0700

    neighBindings-cont

--- perc01an-master/XamarinForms.LocationService/Messages/LocationMessage.cs
+++ perc01an-master/XamarinForms.LocationService/Messages/LocationMessage.cs
@@ -13,8 +13,38 @@
         public string SsidNeighFive { get; set; }
         public string SsidNeighSix { get; set; }
         public string SsidNeighSeven { get; set; }
-
         public string Scanning { get; set; }
-
+        public string x0 { get; set;}
+        public string x1 { get; set; }
+        public string x2 { get; set; }
+        public string x3 { get; set; }
+        public string x4 { get; set; }
+        public string x5 { get; set; }
+        public string x6 { get; set; }
+        public string x7 { get; set; }
+        public string y0 { get; set; }
+        public string y1 { get; set; }
+        public string y2 { get; set; }
+        public string y3 { get; set; }
+        public string y4 { get; set; }
+        public string y5 { get; set; }
+        public string y6 { get; set; }
+        public string y7 { get; set; }
+        public string _X0 { get; set; }
+        public string _X1 { get; set; }
+        public string _X2 { get; set; }
+        public string _X3 { get; set; }
+        public string _X4 { get; set; }
+        public string _X5 { get; set; }
+        public string _X6 { get; set; }
+        public string _X7 { get; set; }
+        public string _Y0 { get; set; }
+        public string _Y1 { get; set; }
+        public string _Y2 { get; set; }
+        public string _Y3 { get; set; }
+        public string _Y4 { get; set; }
+        public string _Y5 { get; set; }
+        public string _Y6 { get; set; }
+        public string _Y7 { get; set; }
     }
 }
--- perc01an-master/XamarinForms.LocationService/ViewModels/MainPageViewModel.cs
+++ perc01an-master/XamarinForms.LocationService/ViewModels/MainPageViewModel.cs
@@ -21,10 +21,44 @@ namespace XamarinForms.LocationService.ViewModels
         private string ssidneighsix;
         private string ssidneighseven;
         private string scanning;
-        public string userMessage;
 
-        public bool startEnabled;
-        public bool stopEnabled;
+        private string x0;
+        private string x1;
+        private string x2;
+        private string x3;
+        private string x4;
+        private string x5;
+        private string x6;
+        private string x7;
+        private string y0;
+        private string y1;
+        private string y2;
+        private string y3;
+        private string y4;
+        private string y5;
+        private string y6;
+        private string y7;
+        private string _x0;
+        private string _x1;
+        private string _x2;
+        private string _x3;
+        private string _x4;
+        private string _x5;
+        private string _x6;
+        private string _x7;
+        private string _y0;
+        private string _y1;
+        private string _y2;
+        private string _y3;
+        private string _y4;
+        private string _y5;
+        private string _y6;
+        private string _y7;
+       
+
+        private string userMessage;
+        private bool startEnabled;
+        private bool stopEnabled;
         #endregion vars
 
         #region properties
@@ -104,6 +138,167 @@ namespace XamarinForms.LocationService.ViewModels
             get => scanning;
             set => SetProperty(ref scanning, value);
         }
+        public string X0
+        {
+            get => x0;
+            set => SetProperty(ref x0, value);
+        }
+        public string X1
+        {
+            get => x1;
+            set => SetProperty(ref x1, value);
+        }
+        public string X2
+        {
+            get => x2;
+            set => SetProperty(ref x2, value);
+        }
+        public string X3
+        {
+            get => x3;
+            set => SetProperty(ref x3, value);
+        }
+        public string X4
+        {
+            get => x4;
+            set => SetProperty(ref x4, value);
+        }
+        public string X5
+        {
+            get => x5;
+            set => SetProperty(ref x5, value);
+        }
+        public string X6
+        {
+            get => x6;
+            set => SetProperty(ref x6, value);
+        }
+        public string X7
+        {
+            get => x7;
+            set => SetProperty(ref x7, value);
+        }
+        public string Y0
+        {
+            get => y0;
+            set => SetProperty(ref y0, value);
+        }
+        public string Y1
+        {
+            get => y1;
+            set => SetProperty(ref y1, value);
+        }
+        public string Y2
+        {
+            get => y2;
+            set => SetProperty(ref y2, value);
+        }
+        public string Y3
+        {
+            get => y3;
+            set => SetProperty(ref y3, value);
+        }
+        public string Y4
+        {
+            get => y4;
+            set => SetProperty(ref y4, value);
+        }
+        public string Y5
+        {
+            get => y5;
+            set => SetProperty(ref y5, value);
+        }
+        public string Y6
+        {
+            get => y6;
+            set => SetProperty(ref y6, value);
+        }
+        public string Y7
+        {
+            get => y7;
+            set => SetProperty(ref y7, value);
+        }
+
+        public string _X0
+        {
+            get => _x0;
+            set => SetProperty(ref _x0, value);
+        }
+        public string _X1
+        {
+            get => _x1;
+            set => SetProperty(ref _x1, value);
+        }
+        public string _X2
+        {
+            get => _x2;
+            set => SetProperty(ref _x2, value);
+        }
+        public string _X3
+        {
+            get => _x3;
+            set => SetProperty(ref _x3, value);
+        }
+        public string _X4
+        {
+            get => _x4;
+            set => SetProperty(ref _x4, value);
+        }
+        public string _X5
+        {
+            get => _x5;
+            set => SetProperty(ref _x5, value);
+        }
+        public string _X6
+        {
+            get => _x6;
+            set => SetProperty(ref _x6, value);
+        }
+        public string _X7
+        {
+            get => _x7;
+            set => SetProperty(ref _x7, value);
+        }
+        public string _Y0
+        {
+            get => _y0;
+            set => SetProperty(ref _y0, value);
+        }
+        public string _Y1
+        {
+            get => _y1;
+            set => SetProperty(ref _y1, value);
+        }
+        public string _Y2
+        {
+            get => _y2;
+            set => SetProperty(ref _y2, value);
+        }
+        public string _Y3
+        {
+            get => _y3;
+            set => SetProperty(ref _y3, value);
+        }
+        public string _Y4
+        {
+            get => _y4;
+            set => SetProperty(ref _y4, value);
+        }
+        public string _Y5
+        {
+            get => _y5;
+            set => SetProperty(ref _y5, value);
+        }
+        public string _Y6
+        {
+            get => _y6;
+            set => SetProperty(ref _y6, value);
+        }
+        public string _Y7
+        {
+            get => _y7;
+            set => SetProperty(ref _y7, value);
+        }
         #endregion properties
 
         #region commands
@@ -182,6 +377,7 @@ namespace XamarinForms.LocationService.ViewModels
                     SsidNeighSeven = message.SsidNeighSeven;
                     UserMessage = "Location Updated";
                     Scanning = message.Scanning;
+                    x0 = message.x0;
                 });
             });
             MessagingCenter.Subscribe<StopServiceMessage>(this, "ServiceStopped", message => {
